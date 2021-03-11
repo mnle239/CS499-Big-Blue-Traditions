ea6d4cd182188e6b4432add5df5036b0270a8319
Molly <mnle239@uky.edu>
2021-03-11 17:36:37 -0500

    connection to mysql database

---
+++ traditionsPage/db_pdo.php
@@ -0,0 +1,6 @@
+<?php
+    define('K_USERNAME', 'mnle239');
+    define('K_PASSWORD', 'u2373099');
+    define('K_CONNECTION_STRING', 'mysql:host=mysql.cs.uky.edu;port=3306;dbname=mnle239');
+    
+?>
