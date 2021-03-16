726d0fdb8a1449e5afeef9d73fa3979a7f1b68d9
Molly <mnle239@uky.edu>
2021-03-16 10:22:25 -0400

    Added Laravel Framework

--- traditionsPage/db_pdo.php
+++
@@ -1,6 +0,0 @@
-<?php
-    define('K_USERNAME', 'mnle239');
-    define('K_PASSWORD', 'u2373099');
-    define('K_CONNECTION_STRING', 'mysql:host=mysql.cs.uky.edu;port=3306;dbname=mnle239');
-    
-?>
