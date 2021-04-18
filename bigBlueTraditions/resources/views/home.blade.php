16498797615f89e6e7f54551f2f19a20c7c0ab2f
Molly <mnle239@uky.edu>
2021-04-18 19:13:42 -0400

    Changed home to match other pages

--- bigBlueTraditions/resources/views/home.blade.php
+++ bigBlueTraditions/resources/views/home.blade.php
@@ -1,13 +1,17 @@
 @extends('layout.app')
 
 @section('content')
-    <img src="{{url('stadium.jpg')}}" alt="" style="float:right">
-    <b>Welcome to the Big Blue Traditions Checklist!</b>
-    <br></br>
-    <b>About:</b>
-    <br></br>
-    Click "traditions" to see the list of upcoming traditions at UK.
-    <br></br>
-    Submit your completed tradition to earn points toward prizes!
-    <br></br>
+    <div class="flex justify-center">
+        <div class="w-8/12 bg-white p-6 rounded-lg">
+            <img src="{{url('stadium.jpg')}}" alt="" style="float:right">
+            <b>Welcome to the Big Blue Traditions Checklist!</b>
+            <br></br>
+            <b>About:</b>
+            <br></br>
+            Click "traditions" to see the list of upcoming traditions at UK.
+            <br></br>
+            Submit your completed tradition to earn points toward prizes!
+            <br></br>
+        </div>
+    </div>
 @endsection
